3f4e9fd37a3bf4d8608d0b0bb08ecbca2a08710c
sindeyz <sindeyz@yandex.ru>
2024-09-12 20:41:32 +0300

    chore: server check 3

--- internal/app.go
+++ internal/app.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
+	"tenders/internal/application/service"
+	"tenders/internal/infrastructure/config"
+	"tenders/internal/infrastructure/persistence"
 	"tenders/internal/interfaces/handlers"
 	"tenders/internal/interfaces/middleware"
 )
@@ -13,42 +15,43 @@ func Run() {
 	mux := http.NewServeMux()
 	handler := middleware.Logging(mux)
 
-	//dbConf := config.NewConfig().PostgresConfig()
-	//repositories := persistence.NewRepositories(config.NewPostgresConn(dbConf))
-	//
-	//tenderService := service.NewTenderService(repositories.TenderRepo, repositories.EmployeeRepo)
-	//tenderHandler := handlers.NewTenderHandler(tenderService)
-	//
-	//bidService := service.NewBidService(repositories.BidRepo, repositories.TenderRepo, repositories.EmployeeRepo)
-	//bidHandler := handlers.NewBidHandler(bidService)
-	//
-	mux.HandleFunc("GET /api/ping", handlers.Ping)
-	//
-	//// tenders
-	//mux.HandleFunc("POST /api/tenders/new", tenderHandler.CreateTender)
-	//mux.HandleFunc("GET /api/tenders", tenderHandler.GetAllTenders)
-	//mux.HandleFunc("GET /api/tenders/my", tenderHandler.GetAllTendersByUsername)
-	//mux.HandleFunc("GET /api/tenders/{tenderId}/status", tenderHandler.GetTenderStatusById)
-	//mux.HandleFunc("PUT /api/tenders/{tenderId}/status", tenderHandler.UpdateTenderStatusById)
-	//mux.HandleFunc("PATCH /api/tenders/{tenderId}/edit", tenderHandler.EditTender)
-	//mux.HandleFunc("PUT /api/tenders/{tenderId}/rollback/{version}", tenderHandler.RollbackTender)
-	//
-	//// bids
-	//mux.HandleFunc("POST /api/bids/new", bidHandler.CreateBid)
-	//mux.HandleFunc("GET /api/bids/my", bidHandler.GetAllBidsByUsername)
-	//
-	//mux.HandleFunc("GET /api/bids/{tenderId}/list", bidHandler.GetAllBidsByUsername)
-	//mux.HandleFunc("GET /api/bids/{bidId}/status", handlers.Ping)
-	//mux.HandleFunc("PUT /api/bids/{bidId}/status", handlers.Ping)
-	//mux.HandleFunc("PATCH /api/bids/{bidId}/edit", handlers.Ping)
-	//mux.HandleFunc("PUT /api/bids/{bidId}/submit_decision", handlers.Ping)
-	//mux.HandleFunc("PUT /api/bids/{bidId}/rollback/{version}", handlers.Ping)
+	dbConf := config.NewConfig().PostgresConfig()
+	repositories := persistence.NewRepositories(config.NewPostgresConn(dbConf))
 
-	serverAddress := os.Getenv("SERVER_ADDRESS")
-	if serverAddress == "" {
-		serverAddress = ":8080"
-	}
-	fmt.Printf(fmt.Sprintf("Starting server on http://%s/\n", serverAddress))
+	tenderService := service.NewTenderService(repositories.TenderRepo, repositories.EmployeeRepo)
+	tenderHandler := handlers.NewTenderHandler(tenderService)
+
+	bidService := service.NewBidService(repositories.BidRepo, repositories.TenderRepo, repositories.EmployeeRepo)
+	bidHandler := handlers.NewBidHandler(bidService)
+
+	mux.HandleFunc("GET /api/ping", handlers.Ping)
+
+	// tenders
+	mux.HandleFunc("POST /api/tenders/new", tenderHandler.CreateTender)
+	mux.HandleFunc("GET /api/tenders", tenderHandler.GetAllTenders)
+	mux.HandleFunc("GET /api/tenders/my", tenderHandler.GetAllTendersByUsername)
+	mux.HandleFunc("GET /api/tenders/{tenderId}/status", tenderHandler.GetTenderStatusById)
+	mux.HandleFunc("PUT /api/tenders/{tenderId}/status", tenderHandler.UpdateTenderStatusById)
+	mux.HandleFunc("PATCH /api/tenders/{tenderId}/edit", tenderHandler.EditTender)
+	mux.HandleFunc("PUT /api/tenders/{tenderId}/rollback/{version}", tenderHandler.RollbackTender)
+
+	// bids
+	mux.HandleFunc("POST /api/bids/new", bidHandler.CreateBid)
+	mux.HandleFunc("GET /api/bids/my", bidHandler.GetAllBidsByUsername)
+
+	mux.HandleFunc("GET /api/bids/{tenderId}/list", bidHandler.GetAllBidsByUsername)
+	mux.HandleFunc("GET /api/bids/{bidId}/status", handlers.Ping)
+	mux.HandleFunc("PUT /api/bids/{bidId}/status", handlers.Ping)
+	mux.HandleFunc("PATCH /api/bids/{bidId}/edit", handlers.Ping)
+	mux.HandleFunc("PUT /api/bids/{bidId}/submit_decision", handlers.Ping)
+	mux.HandleFunc("PUT /api/bids/{bidId}/rollback/{version}", handlers.Ping)
+
+	//serverAddress := os.Getenv("SERVER_ADDRESS")
+	//if serverAddress == "" {
+	//	serverAddress = ":8080"
+	//}
+	//fmt.Printf(fmt.Sprintf("Starting server on http://%s/\n", serverAddress))
+	fmt.Printf("Starting server on http://127.0.0.1:8080/\n")
 
 	if err := http.ListenAndServe(":8080", handler); err != nil {
 		log.Fatal(err)
--- internal/interfaces/handlers/ping_handler.go
+++ internal/interfaces/handlers/ping_handler.go
@@ -3,12 +3,22 @@ package handlers
 import (
 	"fmt"
 	"net/http"
+	"os"
 	"tenders/internal/utils/consts"
 )
 
 func Ping(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain")
-	_, err := fmt.Fprintln(w, "ok")
+	_, err := fmt.Fprintln(w, "ok\n"+
+		os.Getenv("SERVER_ADDRESS")+"\n"+
+		os.Getenv("POSTGRES_CONN")+"\n"+
+		os.Getenv("POSTGRES_JDBC_URL")+"\n"+
+		os.Getenv("POSTGRES_USERNAME")+"\n"+
+		os.Getenv("POSTGRES_PASSWORD")+"\n"+
+		os.Getenv("POSTGRES_HOST")+"\n"+
+		os.Getenv("POSTGRES_PORT")+"\n"+
+		os.Getenv("POSTGRES_DATABASE")+"\n"+
+		"values")
 	if err != nil {
 		http.Error(w, consts.FailedToWriteResponse, http.StatusInternalServerError)
 		return
