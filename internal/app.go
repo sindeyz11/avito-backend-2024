0ecf9a402fa035f74bec350a9fc12cae9a5efc6e
sindeyz <sindeyz@yandex.ru>
2024-09-12 20:31:00 +0300

    chore: server check

--- internal/app.go
+++ internal/app.go
@@ -5,9 +5,6 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"tenders/internal/application/service"
-	"tenders/internal/infrastructure/config"
-	"tenders/internal/infrastructure/persistence"
 	"tenders/internal/interfaces/handlers"
 	"tenders/internal/interfaces/middleware"
 )
@@ -16,36 +13,36 @@ func Run() {
 	mux := http.NewServeMux()
 	handler := middleware.Logging(mux)
 
-	dbConf := config.NewConfig().PostgresConfig()
-	repositories := persistence.NewRepositories(config.NewPostgresConn(dbConf))
-
-	tenderService := service.NewTenderService(repositories.TenderRepo, repositories.EmployeeRepo)
-	tenderHandler := handlers.NewTenderHandler(tenderService)
-
-	bidService := service.NewBidService(repositories.BidRepo, repositories.TenderRepo, repositories.EmployeeRepo)
-	bidHandler := handlers.NewBidHandler(bidService)
-
+	//dbConf := config.NewConfig().PostgresConfig()
+	//repositories := persistence.NewRepositories(config.NewPostgresConn(dbConf))
+	//
+	//tenderService := service.NewTenderService(repositories.TenderRepo, repositories.EmployeeRepo)
+	//tenderHandler := handlers.NewTenderHandler(tenderService)
+	//
+	//bidService := service.NewBidService(repositories.BidRepo, repositories.TenderRepo, repositories.EmployeeRepo)
+	//bidHandler := handlers.NewBidHandler(bidService)
+	//
 	mux.HandleFunc("GET /api/ping", handlers.Ping)
-
-	// tenders
-	mux.HandleFunc("POST /api/tenders/new", tenderHandler.CreateTender)
-	mux.HandleFunc("GET /api/tenders", tenderHandler.GetAllTenders)
-	mux.HandleFunc("GET /api/tenders/my", tenderHandler.GetAllTendersByUsername)
-	mux.HandleFunc("GET /api/tenders/{tenderId}/status", tenderHandler.GetTenderStatusById)
-	mux.HandleFunc("PUT /api/tenders/{tenderId}/status", tenderHandler.UpdateTenderStatusById)
-	mux.HandleFunc("PATCH /api/tenders/{tenderId}/edit", tenderHandler.EditTender)
-	mux.HandleFunc("PUT /api/tenders/{tenderId}/rollback/{version}", tenderHandler.RollbackTender)
-
-	// bids
-	mux.HandleFunc("POST /api/bids/new", bidHandler.CreateBid)
-	mux.HandleFunc("GET /api/bids/my", bidHandler.GetAllBidsByUsername)
-
-	mux.HandleFunc("GET /api/bids/{tenderId}/list", bidHandler.GetAllBidsByUsername)
-	mux.HandleFunc("GET /api/bids/{bidId}/status", handlers.Ping)
-	mux.HandleFunc("PUT /api/bids/{bidId}/status", handlers.Ping)
-	mux.HandleFunc("PATCH /api/bids/{bidId}/edit", handlers.Ping)
-	mux.HandleFunc("PUT /api/bids/{bidId}/submit_decision", handlers.Ping)
-	mux.HandleFunc("PUT /api/bids/{bidId}/rollback/{version}", handlers.Ping)
+	//
+	//// tenders
+	//mux.HandleFunc("POST /api/tenders/new", tenderHandler.CreateTender)
+	//mux.HandleFunc("GET /api/tenders", tenderHandler.GetAllTenders)
+	//mux.HandleFunc("GET /api/tenders/my", tenderHandler.GetAllTendersByUsername)
+	//mux.HandleFunc("GET /api/tenders/{tenderId}/status", tenderHandler.GetTenderStatusById)
+	//mux.HandleFunc("PUT /api/tenders/{tenderId}/status", tenderHandler.UpdateTenderStatusById)
+	//mux.HandleFunc("PATCH /api/tenders/{tenderId}/edit", tenderHandler.EditTender)
+	//mux.HandleFunc("PUT /api/tenders/{tenderId}/rollback/{version}", tenderHandler.RollbackTender)
+	//
+	//// bids
+	//mux.HandleFunc("POST /api/bids/new", bidHandler.CreateBid)
+	//mux.HandleFunc("GET /api/bids/my", bidHandler.GetAllBidsByUsername)
+	//
+	//mux.HandleFunc("GET /api/bids/{tenderId}/list", bidHandler.GetAllBidsByUsername)
+	//mux.HandleFunc("GET /api/bids/{bidId}/status", handlers.Ping)
+	//mux.HandleFunc("PUT /api/bids/{bidId}/status", handlers.Ping)
+	//mux.HandleFunc("PATCH /api/bids/{bidId}/edit", handlers.Ping)
+	//mux.HandleFunc("PUT /api/bids/{bidId}/submit_decision", handlers.Ping)
+	//mux.HandleFunc("PUT /api/bids/{bidId}/rollback/{version}", handlers.Ping)
 
 	serverAddress := os.Getenv("SERVER_ADDRESS")
 	if serverAddress == "" {
